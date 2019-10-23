96e5be3d5a839eeed1938314fb733ef5d299e382
phdumaresq <phdumaresq@gmail.com>
2019-10-23 10:13:02 -0400

    standard --fix

--- src/index.js
+++ src/index.js
@@ -12,24 +12,23 @@ while (!p2 && p1 !== p2) {
 }
 
 window.onload = () => {
-  document.getElementById("p1Name").innerText = p1
-  document.getElementById("p2Name").innerText = p2
+  document.getElementById('p1Name').innerText = p1
+  document.getElementById('p2Name').innerText = p2
   let score1 = 0
   let score2 = 0;
 
-  (function playGame(p1, p2) {
-    document.getElementById("win").style.display = "none"
-    document.getElementById("turn").style.display = "inline"
-    document.getElementById("p1Score").innerText = score1
-    document.getElementById("p2Score").innerText = score2
+  (function playGame (p1, p2) {
+    document.getElementById('win').style.display = 'none'
+    document.getElementById('turn').style.display = 'inline'
+    document.getElementById('p1Score').innerText = score1
+    document.getElementById('p2Score').innerText = score2
 
     const game = new Game(p1, p2)
-    const turn = document.getElementById('turn')
     const player = document.getElementById('player')
     player.innerText = game.player
 
     document.querySelectorAll('#tictactoe td').forEach((el) => {
-      el.innerText = ""
+      el.innerText = ''
       el.onclick = (evt) => {
         el.onclick = undefined
         evt.target.innerText = game.sym
@@ -39,27 +38,23 @@ window.onload = () => {
         game.turn(row, col)
 
         if (game.hasWinner()) {
-          document.getElementById("winner").innerText = game.player
-          document.getElementById("win").style.display = "inline"
-          document.getElementById("turn").style.display = "none"
+          document.getElementById('winner').innerText = game.player
+          document.getElementById('win').style.display = 'inline'
+          document.getElementById('turn').style.display = 'none'
 
-          if (game.player === p1)
-            document.getElementById("p1Score").innerText = ++score1
-          else
-            document.getElementById("p2Score").innerText = ++score2
+          if (game.player === p1) { document.getElementById('p1Score').innerText = ++score1 } else { document.getElementById('p2Score').innerText = ++score2 }
 
-          document.getElementById("newGame").style.display = "inline"
-          document.getElementById("newGame").onclick = () => playGame(p1, p2)
+          document.getElementById('newGame').style.display = 'inline'
+          document.getElementById('newGame').onclick = () => playGame(p1, p2)
 
           document.querySelectorAll('td').forEach(el => {
             el.onclick = undefined
-          });
-        }
-        else {
+          })
+        } else {
           game.nextPlayer()
           player.innerText = game.player
         }
       }
     })
-  })(p1, p2);
+  })(p1, p2)
 }
